3f30dc146c05d44507fa9368b1cd2c363d6b2733
Randy <randysim9@gmail.com>
2024-07-06 17:44:17 -0400

    feat: socials bar

---
+++ src/components/SocialsBar.tsx
@@ -0,0 +1,20 @@
+export default function SocialsBar () {
+    return (
+        <div className="relative bottom-0 left-0 z-10 flex w-16 flex-col justify-center">
+            <div className="flex w-20 flex-col">
+                <a href="https://github.com/randysim">
+                    <img src="/github.svg" className="w-10 h-10 my-3 cursor-pointer" />
+                </a>
+                <a href="https://www.linkedin.com/in/randysim/">
+                    <img src="/linkedin.svg" className="w-10 h-10 my-3 cursor-pointer" />
+                </a>
+                <a href="mailto:randysim9@gmail.com" className="relative group">
+                    <img src="/email.svg" className="w-10 h-10 my-3 cursor-pointer group" />
+                    <span className="absolute hidden w-auto p-2 text-sm text-white bg-[#578a65] rounded-md group-hover:block">
+                        randysim9@gmail.com
+                    </span>
+                </a>
+            </div>
+        </div>
+    )
+}
