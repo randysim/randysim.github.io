202364ae789779b46fdafc107bedf0329a01d7a8
Randy <randysim9@gmail.com>
2025-05-31 12:49:47 -0400

    feat: attempt retrogrid fix

--- src/components/magicui/retro-grid.tsx
+++ src/components/magicui/retro-grid.tsx
@@ -54,14 +54,14 @@ export function RetroGrid({
   return (
     <div
       className={cn(
-        "pointer-events-none absolute size-full overflow-hidden [perspective:200px]",
+        "pointer-events-none absolute size-full overflow-hidden [perspective:200px] [transform-style:preserve-3d] [backface-visibility:hidden]",
         `opacity-[var(--opacity)]`,
         className,
       )}
       style={gridStyles}
       {...props}
     >
-      <div className="absolute inset-0 [transform:rotateX(var(--grid-angle))]">
+      <div className="absolute inset-0 [transform:rotateX(var(--grid-angle))] [transform-style:preserve-3d]">
         <div className="animate-grid [background-image:linear-gradient(to_right,var(--light-line)_1px,transparent_0),linear-gradient(to_bottom,var(--light-line)_1px,transparent_0)] [background-repeat:repeat] [background-size:var(--cell-size)_var(--cell-size)] [height:300vh] [inset:0%_0px] [margin-left:-200%] [transform-origin:100%_0_0] [width:600vw] dark:[background-image:linear-gradient(to_right,var(--dark-line)_1px,transparent_0),linear-gradient(to_bottom,var(--dark-line)_1px,transparent_0)]" />
       </div>
 
